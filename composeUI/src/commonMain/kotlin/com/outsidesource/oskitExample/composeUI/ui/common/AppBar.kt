fb6a0cc37a779ff7df5f5be8460fa4f7037e24d3
Ryan Mitchener <ryan@outsidesource.com>
2023-09-01 00:14:13 -0400

    Added insets

--- composeUI/src/commonMain/kotlin/com/outsidesource/oskitExample/composeUI/ui/common/AppBar.kt
+++ composeUI/src/commonMain/kotlin/com/outsidesource/oskitExample/composeUI/ui/common/AppBar.kt
@@ -1,16 +1,21 @@
 package com.outsidesource.oskitExample.composeUI.ui.common
 
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.outsidesource.oskitcompose.systemui.KMPWindowInsets
+import com.outsidesource.oskitcompose.systemui.verticalInsets
 
 @Composable
 fun AppBar(title: String, hasBackStack: Boolean, onBackPress: () -> Unit) {
     TopAppBar(
+        modifier = Modifier
+            .background(MaterialTheme.colors.primary)
+            .windowInsetsPadding(KMPWindowInsets.verticalInsets),
         title = { Text(title) },
         navigationIcon = if (!hasBackStack) null else { {
             IconButton(onClick = onBackPress) {
