d53626ead9b4368fb53d580be9036d9156190bfb
Ryan Mitchener <ryan@outsidesource.com>
2023-08-31 23:52:14 -0400

    Started Koin

--- composeUI/src/iosMain/kotlin/main.ios.kt
+++ composeUI/src/iosMain/kotlin/main.ios.kt
@@ -1,4 +1,8 @@
 import androidx.compose.ui.window.ComposeUIViewController
+import com.outsidesource.oskitExample.common.initKoin
+import com.outsidesource.oskitExample.composeUI.composeUIModule
 import com.outsidesource.oskitExample.composeUI.ui.app.App
+
+private val koin = initKoin(extraModules = composeUIModule.toTypedArray()).koin
 
 fun MainViewController() = ComposeUIViewController { App() }
