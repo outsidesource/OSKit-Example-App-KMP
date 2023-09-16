c45cbd8468183e8a5787628eac4ce344f1a73f38
Ryan Mitchener <ryan@outsidesource.com>
2023-09-16 00:47:17 -0400

    Added images and fonts

---
+++ composeUI/src/commonMain/kotlin/com/outsidesource/oskitExample/composeUI/Resources.kt
@@ -0,0 +1,28 @@
+package com.outsidesource.oskitExample.composeUI
+
+import androidx.compose.ui.text.font.FontWeight
+import com.outsidesource.oskitcompose.resources.KMPFont
+import com.outsidesource.oskitcompose.resources.KMPFontFamily
+import com.outsidesource.oskitcompose.resources.KMPImage
+import com.outsidesource.oskitcompose.resources.resolveKmpFontFamily
+import kotlinx.coroutines.runBlocking
+
+object Images {
+    val Penguin = KMPImage("images/penguin.png")
+    val TuxXML = KMPImage("images/tux.xml")
+    val TuxMonoXML = KMPImage("images/tux-mono.xml")
+}
+
+object Fonts {
+    val SansSerifPro by lazy {
+        val family = KMPFontFamily(
+            listOf(
+                KMPFont("fonts/SourceSansPro-Bold.ttf", weight = FontWeight.Bold),
+                KMPFont("fonts/SourceSansPro-Light.ttf", weight = FontWeight.Light),
+                KMPFont("fonts/SourceSansPro-Regular.ttf", weight = FontWeight.Normal),
+                KMPFont("fonts/SourceSansPro-SemiBole.ttf", weight = FontWeight.SemiBold)
+            )
+        )
+        runBlocking { resolveKmpFontFamily(family) }
+    }
+}
