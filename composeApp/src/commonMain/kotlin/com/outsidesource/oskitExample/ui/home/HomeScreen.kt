3eb8632311a91edb0e792399e2faca591e7911a1
Ryan Mitchener <ryan@outsidesource.com>
2024-02-20 03:34:02 -0500

    Began work on wheel picker

--- composeApp/src/commonMain/kotlin/com/outsidesource/oskitExample/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/outsidesource/oskitExample/ui/home/HomeScreen.kt
@@ -1,19 +1,41 @@
 package com.outsidesource.oskitExample.ui.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.MutatePriority
+import androidx.compose.foundation.gestures.*
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyListItemInfo
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.Saver
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.util.VelocityTracker
+import androidx.compose.ui.input.pointer.util.addPointerInputChange
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
-import com.outsidesource.oskitExample.resources.Strings
 import com.outsidesource.oskitExample.ui.common.Screen
 import com.outsidesource.oskitcompose.lib.rememberInjectForRoute
-import com.outsidesource.oskitcompose.resources.rememberKmpString
+import com.outsidesource.oskitcompose.pointer.awaitFirstUp
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+import kotlin.math.abs
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     interactor: HomeViewInteractor = rememberInjectForRoute(),
@@ -24,34 +46,282 @@ fun HomeScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically)
         ) {
-            Button(
-                content = { Text(rememberKmpString(Strings.appInteractorExample)) },
-                onClick = interactor::appStateExampleButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.viewInteractorExample)) },
-                onClick = interactor::viewStateExampleButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.fileHandling)) },
-                onClick = interactor::fileHandlingButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.markdown)) },
-                onClick = interactor::markdownButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.popups)) },
-                onClick = interactor::popupsButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.resources)) },
-                onClick = interactor::resourcesButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.iosServices)) },
-                onClick = interactor::iosServicesButtonClicked,
-            )
+//            Button(
+//                content = { Text(rememberKmpString(Strings.appInteractorExample)) },
+//                onClick = interactor::appStateExampleButtonClicked,
+//            )
+//            Button(
+//                content = { Text(rememberKmpString(Strings.viewInteractorExample)) },
+//                onClick = interactor::viewStateExampleButtonClicked,
+//            )
+//            Button(
+//                content = { Text(rememberKmpString(Strings.fileHandling)) },
+//                onClick = interactor::fileHandlingButtonClicked,
+//            )
+//            Button(
+//                content = { Text(rememberKmpString(Strings.markdown)) },
+//                onClick = interactor::markdownButtonClicked,
+//            )
+//            Button(
+//                content = { Text(rememberKmpString(Strings.popups)) },
+//                onClick = interactor::popupsButtonClicked,
+//            )
+//            Button(
+//                content = { Text(rememberKmpString(Strings.resources)) },
+//                onClick = interactor::resourcesButtonClicked,
+//            )
+//            Button(
+//                content = { Text(rememberKmpString(Strings.iosServices)) },
+//                onClick = interactor::iosServicesButtonClicked,
+//            )
+
+            val wheelPickerState = remember { WheelPickerState(isInfinite = true, initiallySelectedItemIndex = 4) }
+
+            WheelPicker(
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(175.dp),
+                items = (0..105).toList(),
+                key = { it },
+                state = wheelPickerState,
+                onChange = {
+                    println("On Change: $it")
+                }
+            ) {
+                Text(
+                    modifier = Modifier
+                        .height(25.dp)
+                        .fillMaxWidth(),
+                    text = it.toString(),
+                    textAlign = TextAlign.Center,
+                )
+            }
         }
     }
 }
+
+
+@Stable
+data class WheelPickerState(
+    val initiallySelectedItemIndex: Int = 0,
+    val isInfinite: Boolean = true,
+): ScrollableState {
+    val lazyListState: LazyListState = LazyListState(
+        firstVisibleItemIndex = if (isInfinite) {
+            INFINITE_OFFSET + initiallySelectedItemIndex
+        } else {
+            initiallySelectedItemIndex
+        }
+    )
+
+    internal val itemHeight by derivedStateOf {
+        lazyListState.layoutInfo.visibleItemsInfo.firstOrNull()?.size ?: 0
+    }
+
+    internal val viewportHeight by derivedStateOf {
+        lazyListState.layoutInfo.viewportSize.height
+    }
+
+    internal val selectedItemIndex by derivedStateOf {
+        (if (isInfinite) {
+            selectedItem?.index?.minus(INFINITE_OFFSET)
+        } else {
+            selectedItem?.index
+        }) ?: initiallySelectedItemIndex
+    }
+
+    internal val selectedItem by derivedStateOf {
+        val layoutInfo = lazyListState.layoutInfo
+        layoutInfo.visibleItemsInfo.firstOrNull {
+            it.offset + it.size - layoutInfo.viewportStartOffset > layoutInfo.viewportSize.height / 2
+        }
+    }
+
+    // TODO: Fix this (rename this is for the animation)
+    internal val selectedItemIndex2 by derivedStateOf {
+        (if (isInfinite) {
+            selectedItem2?.index?.minus(INFINITE_OFFSET)
+        } else {
+            selectedItem2?.index
+        }) ?: initiallySelectedItemIndex
+    }
+
+    // TODO: Fix this (rename this is for the animation)
+    internal val selectedItem2 by derivedStateOf {
+        val layoutInfo = lazyListState.layoutInfo
+        layoutInfo.visibleItemsInfo.firstOrNull {
+            it.offset + it.size - layoutInfo.viewportStartOffset > (layoutInfo.viewportSize.height / 2) - (it.size / 2)
+        }
+    }
+
+    override val isScrollInProgress: Boolean
+        get() = lazyListState.isScrollInProgress
+
+    override val canScrollBackward: Boolean
+        get() = lazyListState.canScrollBackward
+
+    override val canScrollForward: Boolean
+        get() = lazyListState.canScrollForward
+
+    override suspend fun scroll(scrollPriority: MutatePriority, block: suspend ScrollScope.() -> Unit) =
+        lazyListState.scroll(scrollPriority, block)
+
+    override fun dispatchRawDelta(delta: Float): Float = lazyListState.dispatchRawDelta(delta)
+
+    suspend fun scrollToItem(index: Int) {
+        lazyListState.scrollToItem(if (isInfinite) INFINITE_OFFSET + index else index)
+    }
+
+    suspend fun animateScrollToItem(index: Int) = lazyListState.animateScrollToItem(index)
+
+    companion object {
+        fun Saver(): Saver<WheelPickerState, *> = Saver(
+            save = { listOf(it.isInfinite, it.selectedItemIndex) },
+            restore = {
+                WheelPickerState(
+                    isInfinite = it[0] as Boolean,
+                    initiallySelectedItemIndex = it[1] as Int
+                )
+            }
+        )
+    }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun <T : Any> WheelPicker(
+    items: List<T>,
+    key: (T) -> Any,
+    state: WheelPickerState,
+    modifier: Modifier = Modifier,
+    enabled : Boolean = true,
+    onChange: (T) -> Unit,
+    content: @Composable LazyItemScope.(T) -> Unit
+) {
+    val velocityTracker = remember { VelocityTracker() }
+    val scope = rememberCoroutineScope()
+    val flingBehavior = rememberSnapFlingBehavior(state.lazyListState)
+
+    val paddingValues = with(LocalDensity.current) {
+        remember(state.lazyListState.layoutInfo.viewportSize) {
+            val viewportHeight = state.lazyListState.layoutInfo.viewportSize.height.toDp()
+            val itemHeight = state.lazyListState.layoutInfo.visibleItemsInfo.firstOrNull()?.size?.toDp() ?: 0.dp
+            PaddingValues(vertical = (((viewportHeight) - itemHeight) / 2).coerceAtLeast(0.dp))
+        }
+    }
+
+    // TODO: Both pointerInputs should only happen on desktop, but need to make sure that scroll cancellation and letting go still adjusts
+    // TODO: Add scroll wheel debouncer to fix scroll after it's done or disable scroll
+    // TODO: Need to add indicators
+    // TODO: Implement saver
+    // TODO: Add alpha mask
+    // TODO: Need to snap on initial load
+    LazyColumn(
+        modifier = modifier
+            .pointerInput(Unit) {
+                awaitEachGesture {
+                    awaitFirstDown()
+                    scope.launch {
+                        state.lazyListState.scrollBy(0f)
+                    }
+
+                    awaitFirstUp()
+                    scope.launch {
+                        if (state.lazyListState.isScrollInProgress) return@launch // Is flinging
+                        state.lazyListState.scroll {
+                            with(flingBehavior) {
+                                performFling(0f)
+                                onChange(items[getItemsIndex(state.selectedItemIndex, state, items)])
+                            }
+                        }
+                    }
+                }
+            }
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragStart = {
+                        velocityTracker.resetTracking()
+                    },
+                    onDrag = { change, delta ->
+                        velocityTracker.addPointerInputChange(change)
+                        state.lazyListState.dispatchRawDelta(-delta.y)
+                    },
+                    onDragEnd = {
+                        val velocity = -velocityTracker.calculateVelocity().y
+                        scope.launch {
+                            state.lazyListState.scroll {
+                                with(flingBehavior) {
+                                    performFling(velocity)
+                                    onChange(items[getItemsIndex(state.selectedItemIndex, state, items)])
+                                }
+                            }
+                        }
+                    },
+                )
+            }
+            .drawWithContent {
+                drawRoundRect(
+                    color = Color(0x22000000),
+                    topLeft = Offset(0f, (size.height / 2) - (14f * density)),
+                    size = Size(size.width, 25 * density),
+                    cornerRadius = CornerRadius(20f, 20f)
+                )
+                drawContent()
+            },
+        userScrollEnabled = enabled,
+        state = state.lazyListState,
+        contentPadding = paddingValues,
+        flingBehavior = flingBehavior,
+    ) {
+        items(
+            count = if (state.isInfinite) Int.MAX_VALUE else items.size,
+            key = { key(items[getLazyListIndex(it, state, items)]) }
+        ) { index ->
+            Box(
+                modifier = Modifier
+//                    .pointerInput(Unit) {
+//                        detectTapGestures {
+//                            scope.launch {
+//                                // TODO: This is wrong
+//                                state.lazyListState.animateScrollToItem(getInfiniteIndex(index, state, items))
+//                            }
+//                        }
+//                    }
+                    .graphicsLayer {
+                        val adjust = if (state.isInfinite) INFINITE_OFFSET else 0
+                        val stepsFromSelected = index - adjust - state.selectedItemIndex2 // Halfway point
+                        val offset = state.lazyListState.firstVisibleItemScrollOffset.toFloat() // When bottom reaches top
+                        val pixelsFromSelected = (stepsFromSelected.toFloat() * state.itemHeight.toFloat()) - offset
+                        val maxPixelsFromSelected = (state.viewportHeight.toFloat() / 2) + (state.itemHeight.toFloat() / 2)
+                        val mult = (pixelsFromSelected / maxPixelsFromSelected)
+
+                        rotationX = 60f * mult
+                        scaleX = 1f - .5f * abs(mult)
+                        scaleY = 1f - .5f * abs(mult)
+                        alpha = 1f - .5f * abs(mult)
+                    }
+            ) {
+                content(items[getLazyListIndex(index, state, items)])
+            }
+        }
+    }
+}
+
+private fun <T> getLazyListIndex(index: Int, state: WheelPickerState, items: List<T>) =
+    if (state.isInfinite) {
+        val mod = ((index - INFINITE_OFFSET) % items.size)
+        if (mod < 0) items.size + mod else mod
+    } else {
+        index
+    }
+
+private fun <T> getItemsIndex(index: Int, state: WheelPickerState, items: List<T>) =
+    if (state.isInfinite) {
+        val mod = index % items.size
+        if (mod < 0) items.size + mod else mod
+    } else {
+        index
+    }
+
+private const val INFINITE_OFFSET = Int.MAX_VALUE / 2
