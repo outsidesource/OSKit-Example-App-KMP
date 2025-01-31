be3d02edc1f349ac9c2a99eeb54147a77c1106c6
Ryan Mitchener <ryan@outsidesource.com>
2025-01-31 17:21:42 -0500

    Added scrolling to home screen

--- composeApp/src/commonMain/kotlin/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/home/HomeScreen.kt
@@ -1,8 +1,13 @@
 package ui.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,53 +18,70 @@ import resources.Strings
 import ui.common.Screen
 import com.outsidesource.oskitcompose.lib.rememberInjectForRoute
 import com.outsidesource.oskitcompose.resources.rememberKmpString
+import com.outsidesource.oskitcompose.scrollbars.KmpVerticalScrollbar
+import com.outsidesource.oskitcompose.scrollbars.rememberKmpScrollbarAdapter
+import ui.app.theme.AppTheme
 
 @Composable
 fun HomeScreen(
     interactor: HomeViewInteractor = rememberInjectForRoute(),
 ) {
-    Screen("Home") {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically)
-        ) {
-            Button(
-                content = { Text(rememberKmpString(Strings.appInteractorExample)) },
-                onClick = interactor::appStateExampleButtonClicked,
+    Screen(
+        title = "Home",
+        paddingValues = PaddingValues(0.dp),
+    ) {
+        val scrollState = rememberScrollState()
+
+        Box {
+            KmpVerticalScrollbar(
+                modifier = Modifier.align(Alignment.TopEnd),
+                adapter = rememberKmpScrollbarAdapter(scrollState)
             )
-            Button(
-                content = { Text(rememberKmpString(Strings.viewInteractorExample)) },
-                onClick = interactor::viewStateExampleButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.fileSystem)) },
-                onClick = interactor::fileHandlingButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.markdown)) },
-                onClick = interactor::markdownButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.popups)) },
-                onClick = interactor::popupsButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.resources)) },
-                onClick = interactor::resourcesButtonClicked,
-            )
-            Button(
-                content = { Text(rememberKmpString(Strings.iosServices)) },
-                onClick = interactor::iosServicesButtonClicked,
-            )
-            Button(
-                content = { Text("Widgets") },
-                onClick = interactor::widgetsButtonClicked,
-            )
-            Button(
-                content = { Text("Capability") },
-                onClick = interactor::capabilityButtonClicked,
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+                    .padding(AppTheme.dimensions.screenPadding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically)
+            ) {
+                Button(
+                    content = { Text(rememberKmpString(Strings.appInteractorExample)) },
+                    onClick = interactor::appStateExampleButtonClicked,
+                )
+                Button(
+                    content = { Text(rememberKmpString(Strings.viewInteractorExample)) },
+                    onClick = interactor::viewStateExampleButtonClicked,
+                )
+                Button(
+                    content = { Text(rememberKmpString(Strings.fileSystem)) },
+                    onClick = interactor::fileHandlingButtonClicked,
+                )
+                Button(
+                    content = { Text(rememberKmpString(Strings.markdown)) },
+                    onClick = interactor::markdownButtonClicked,
+                )
+                Button(
+                    content = { Text(rememberKmpString(Strings.popups)) },
+                    onClick = interactor::popupsButtonClicked,
+                )
+                Button(
+                    content = { Text(rememberKmpString(Strings.resources)) },
+                    onClick = interactor::resourcesButtonClicked,
+                )
+                Button(
+                    content = { Text(rememberKmpString(Strings.iosServices)) },
+                    onClick = interactor::iosServicesButtonClicked,
+                )
+                Button(
+                    content = { Text("Widgets") },
+                    onClick = interactor::widgetsButtonClicked,
+                )
+                Button(
+                    content = { Text("Capability") },
+                    onClick = interactor::capabilityButtonClicked,
+                )
+            }
         }
     }
 }
